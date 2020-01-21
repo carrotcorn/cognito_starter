b6eaa173fc81194659f5e00bcd494c2d1caaee84
Eric Bourne <ericbourne6371@gmail.com>
2020-01-21 10:10:12 -0800

    added navbar and forgot password, updated Login

--- src/App.js
+++ src/App.js
@@ -6,19 +6,62 @@ import Home from './components/Home';
 import Register from './components/auth/Register';
 import Welcome from './components/auth/Welcome';
 import LogIn from './components/auth/LogIn';
+import { Auth } from 'aws-amplify';
+// import ForgotPassword from './components/auth/ForgotPassword';
+// import ForgotPasswordSubmit from './components/auth/ForgotPasswordSubmit';
 
 class App extends Component {
+  state = {
+    isAuth: false,
+    user: null,
+    checkingAuth: true
+  }
+
+  authenticateUser = authenticated => {
+    this.setState({ isAuth: authenticated });
+  }
+
+  setAuthUser = user => {
+    this.setState({ user: user });
+  }
+
+
+  async componentDidMount() {
+    try {
+      const session = await Auth.currentSession();
+      this.authenticateUser(true);
+      console.log(session);
+      const user = await Auth.currentAuthenticatedUser();
+      this.setAuthUser(user);
+    } catch (error) {
+      console.log(error)
+    }
+
+    this.setState({ checkingAuth: false })
+  }
+
   render() {
+    const authProps = {
+      isAuth: this.state.isAuth,
+      user: this.state.user,
+      authenticateUser: this.authenticateUser,
+      setAuthUser: this.setAuthUser
+    }
+
     return (
+      !this.state.checkingAuth &&
       <div className="App">
         <Router>
           <div>
-            <Navbar />
+            <Navbar auth={authProps} />
             <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/register" component={Register} />
-              <Route exact path="/welcome" component={Welcome} />
-              <Route exact path="/login" component={LogIn} />
+              <Route exact path="/" render={(props) => <Home  {...props} auth={authProps} />} />
+              <Route exact path="/register" render={(props) => <Register  {...props} auth={authProps} />} />
+              <Route exact path="/welcome" render={(props) => <Welcome  {...props} auth={authProps} />} />
+              <Route exact path="/login" render={(props) => <LogIn  {...props} auth={authProps} />} />
+
+              <Route exact path="/forgotpassword" render={(props) => <ForgotPassword  {...props} auth={authProps} />} />
+              <Route exact path="/forgotpasswordsubmit" render={(props) => <ForgotPasswordSubmit  {...props} auth={authProps} />} />
             </Switch>
           </div>
         </Router>
